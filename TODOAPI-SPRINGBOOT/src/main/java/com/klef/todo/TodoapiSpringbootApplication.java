f506b7593cb3c6a3b00f164987197c6d0f82d840
ADS <dharshinisaiavula550@gmail.com>
2025-09-09 21:55:30 +0530

    Full Stack Practice Project v1.0

--- TODOAPI-SPRINGBOOT/src/main/java/com/klef/todo/TodoapiSpringbootApplication.java
+++ TODOAPI-SPRINGBOOT/src/main/java/com/klef/todo/TodoapiSpringbootApplication.java
@@ -2,9 +2,10 @@ package com.klef.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TodoapiSpringbootApplication {
+public class TodoapiSpringbootApplication  extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoapiSpringbootApplication.class, args);
